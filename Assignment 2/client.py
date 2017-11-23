8580b7a31ad1c654007db83a1fab187b070fe2ef
Amreen Abdul Wahab <amreenabdulwahab@gmail.com>
2017-11-22 21:30:53 -0800

    Updated client to run with python 3 and docker

--- Assignment 2/client.py
+++ Assignment 2/client.py
@@ -19,24 +19,42 @@ class MainClient():
         return self.stub.put(replicator_pb2.Request(key=key ,value=value))
 
     def delete(self, key):
-        return self.stub.delete(replicator_pb2.DeleteRequest(key=key))
+        return self.stub.delete(replicator_pb2.GetAndDeleteRequest(key=key))
+
+    def get(self, key):
+        return self.stub.get(replicator_pb2.GetAndDeleteRequest(key=key))
 
 def main():
    
-    client = MainClient()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help="display a square of a given number")
+    args = parser.parse_args()
+    print("Client is connecting to Server at {}:{}...".format(args.host, PORT))
+    client = MainClient(host=args.host)
+    print(" ")
+    print("---------------------------------------------------------------")
+    print("")
     print ("# PUT Request 1 :  key = a   value = foo ")
     resp = client.put('a','foo')
-    print ("# PUT Response: key = " +resp.key + "value = "+resp.value)
-    
+    print ("# PUT Response 1 : key = " +resp.key + "value = "+resp.value)
+    print(" ")
+    print("---------------------------------------------------------------")
+    print("")
     print ("# PUT Request 2 :  key = b   value = bar ")
     resp1 = client.put('b','bar')
-    print ("# PUT Response: key = " +resp1.key + "   value = "+resp1.value)
-
-    print("# DELETE Request  1: ")
+    print ("# PUT Response 2 : key = " +resp1.key + "   value = "+resp1.value)
+    print("")
+    print("---------------------------------------------------------------")
+    print("")
+    print("# DELETE Request  1 :  key =  " + resp.key)
     resp2 = client.delete(resp.key)
-    print("# DELETE Response: key deleted = " +resp2.key+"  value deleted = " +resp2.value) 
-
-
+    print("# DELETE Response 1 : key deleted = " +resp2.key+"  value deleted = " +resp2.value) 
+    print("")
+    print("---------------------------------------------------------------")
+    print("")
+    print("# GET Request 1 : key = "+resp1.key)
+    resp3 = client.get(resp1.key)
+    print("# GET Response 1 : key  = " +resp3.key+"  value  = " +resp3.value) 
+    
 if __name__ == "__main__":
     main()
