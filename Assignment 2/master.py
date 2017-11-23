73c022cfcd84b6f2fd850d77b876bd42b6df496e
Amreen Abdul Wahab <amreenabdulwahab@gmail.com>
2017-11-22 21:31:45 -0800

    made code compatible with python 3

--- Assignment 2/master.py
+++ Assignment 2/master.py
@@ -8,8 +8,8 @@ import grpc
 import replicator_pb2
 import replicator_pb2_grpc
 import rocksdb
-
-import Queue
+import queue
+#import Queue
 from concurrent import futures
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
@@ -17,46 +17,60 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 class ReplicatorServicer(replicator_pb2.ReplicatorServicer):
     def __init__(self):
         self.db = rocksdb.DB("masterdb.db", rocksdb.Options(create_if_missing=True))
-        self.queue = Queue.Queue()
+        self.queue= queue.Queue()
 
     def replicate(db_action):
-        def wrapper(self, request, context):
-            
+        #decorator to replicate db operations
+        def wrapper(self, request, context):        
             if(db_action.__name__=="delete"):
                 value = (self.db.get(request.key.encode()))
                 action = replicator_pb2.SlaveResponse(action = db_action.__name__, key=request.key.encode(), value=value)
             else:
                 action = replicator_pb2.SlaveResponse(action = db_action.__name__, key=request.key.encode(), value=request.value.encode())
-            
+            #add db operations to a queue in the form of a response
             self.queue.put(action)
-            print ("DB queue sent to slave")
             return db_action(self, request, context)
-
         return wrapper
+
+
+    def slaveConnector(self, request, context):
+        '''
+        slave connects with master and master sends db_actions from queue to slave
+        busy-wait loop
+        '''
+
+        counter = 1 
+        print ("Slave Connected")
+        while True:
+            while not self.queue.empty():
+                    #send each request to slave from the operations queue
+                    print("Replicating request :  "+ str(counter) +"  to slave")
+                    counter = counter + 1
+                    yield self.queue.get() 
+                    
+
+
 
     @replicate
     def put(self, request, context):
-        print ("put")
-        #key = uuid.uuid4().hex
+        print ("## PUT REQUEST")
+        #add data into client db
         self.db.put(request.key.encode(), request.value.encode())
         return replicator_pb2.Response(key=request.key,value=request.value)
     
     @replicate
     def delete(self, request, context):
-        print ("delete")
+        print ("## DELETE REQUEST")
         value = (self.db.get(request.key.encode())).decode()
+        #delete data from client db
         self.db.delete(request.key.encode())
         return replicator_pb2.Response(key=request.key, value=value)
 
-    def slaveConnector(self, request, context):
-        '''
-        slave connects with master and master sends db_actions from queue to slave
-        '''
-        print ("Slave Connected")
-        while True:
-            while not self.queue.empty():
-                    yield self.queue.get() 
-                    print ("Successful replication of data on slave side") 
+    def get(self, request, context):
+        print ("## GET REQUEST")
+        #fetch data from client db
+        value = (self.db.get(request.key.encode())).decode()
+        return replicator_pb2.Response(key=request.key, value=value)
 
 
 def run(host, port):
